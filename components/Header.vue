24d7d616b8266017be7115b9fe5224f03c98b9cc
skiboorg <d@skib.org>
2021-06-27 19:20:32 +0300

    add pack

--- components/Header.vue
+++ components/Header.vue
@@ -1,10 +1,8 @@
 <template>
   <div class="">
-    <div :class="{'header-w-line' : is_index_page}" >
+
       <header class="header">
-        <div v-if="is_index_page" class="header-line">
-          <p class="text-center">Подпишись и получи скидку 15%</p>
-        </div>
+
         <div class="header-wrapper">
           <div class="header-nav">
             <div class="header-nav__button hide-mob-600 with-submenu"
@@ -62,7 +60,7 @@
               </svg>
             </a>
 
-            <div  class="header-icons__cart" @mouseover="headerCartShow=true" @click="headerCartShow=!headerCartShow">
+            <div @click="$router.push('/cart')" class="header-icons__cart" @mouseover="headerCartShow=true" >
               <svg width="21" height="25" viewBox="0 0 21 25" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M15.3828 6.39648V1.54858L13.8342 0H7.16576L5.61719 1.54858V6.39648H0.0507812V22.0703C0.0507812 23.6858 1.36494 25 2.98047 25H18.0195C19.6351 25 20.9492 23.6858 20.9492 22.0703V6.39648H15.3828ZM18.9961 22.0703C18.9961 22.6088 18.558 23.0469 18.0195 23.0469H2.98047C2.44202 23.0469 2.00391 22.6088 2.00391 22.0703V8.34961H5.61719V11.8652H7.57031V2.35767L7.97486 1.95312H13.0251L13.4297 2.35767V6.39648H9.57227V8.34961H13.4297V11.8652H15.3828V8.34961H18.9961V22.0703Z" fill="#519999"/>
               </svg>
@@ -75,7 +73,7 @@
 
           </div>
 
-          <div @mouseleave="headerCartShow=false" class="header-cart" :class="{'headerCartActive':headerCartShow}">
+          <div  @mouseleave="headerCartShow=false" class="header-cart" :class="{'headerCartActive':headerCartShow}">
             <div @click="headerCartShow=false" class="header-cart__close"><span>&#10005;</span></div>
             <div class="header-cart__item" v-for="(item,index) in cart.items" :key="item.id">
               <img :src="base_url+item.item_type.image" alt="">
@@ -134,7 +132,7 @@
 
         </div>
       </header>
-    </div>
+
     <el-dialog class="authModal"  :visible.sync="authModal" @closed="authModalClosed">
       <div class="login-form">
         <h1 class="login-form__title">{{authModalTitle}}</h1>
